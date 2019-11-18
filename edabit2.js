ebbed0f6abec9cc61b7f9aae8cd7caa512c17b83
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2019-11-18 13:46:39 -0800

    solutions

--- edabit2.js
+++ edabit2.js
@@ -3,7 +3,6 @@ function removeDups(arr) {
   return [...new Set(arr)];
 }
 
-
 //Format Number with Comma(s) Separating Thousands
 
 function formatNum(num) {
@@ -35,3 +34,45 @@ function formatNum(num) {
 function rev(n) {
   return Math.abs(n).toString().split('').reverse().join('')
 }
+
+
+//Odd Up, Even Down — N Times Map
+
+function evenOddTransform(arr, n) {
+  return arr.map(x => (x % 2 == 0) ? x - 2 * n : x + 2 * n);
+}
+// let arr = [3, 4, 9];
+// let n = 3;
+
+// let arr1 = arr.map(function (x) {
+//   if (x % 2 === 0) {
+//     return x - 2 * n;
+//   } else {
+//     return x + 2 * n;
+//   }
+// })
+
+// console.log(arr1); 
+
+//Array of Multiples
+
+function arrayOfMultiples(num, length) {
+  let arr = [];
+  for (let i = 1; i < length + 1; i++) {
+    arr.push(num * i);
+  }
+  return arr;
+}
+
+Array.from({length: length}, (_, i) => num*(i+1));
+
+//Capitalize the Names
+function capMe(arr) {
+  let arr1 = arr.map(function (ele) {
+    let ele1 = ele.split('');
+    return ele1[0].toUpperCase() + ele1.slice(2. - 1).join('').toLowerCase();
+  })
+  return arr1;
+}
+
+//capMe(['cat', 'dog', 'fish']);
--- practice.js
+++ practice.js
@@ -1,32 +1,12 @@
+let arr = [3,4,9];
+let n = 3; 
 
-function countOnes(i) {
-
-  let num = i; 
-  let baseArray = [1];
-
-  for(let j = 2; j < num; j*=2){
-      baseArray.unshift(j);
-  }
-
-  let binaryArr = [];
-  let remainder = num; 
- 
-  for(let k= 0; k< baseArray.length; k++){
-
-    if(remainder >= baseArray[k]){
-       remainder = num % baseArray[k]; 
-       binaryArr.unshift(1); 
+let arr1 = arr.map(function (x) {
+    if (x % 2 === 0) {
+      return x - 2*n;
+    } else {
+      return x + 2*n;
     }
-    else if(remainder < baseArray[k]){
-      binaryArr.unshift(0);
-    }
+})
 
-  }
-
-  console.log(binaryArr);
-  const reducer = (a,b) => a+b; 
-  console.log(binaryArr.reduce(reducer)) ;
-
-}
-
-countOnes(101);
+console.log(arr1); 
